88de78b4e247b34910faaa9164c2d1a45ab24fa9
alvin <alvin@DESKTOP-391GE8T>
2017-08-17 10:52:55 -0400

    Created the ScoreBoard class

---
+++ COMP123-S2017-FinalExam-StudentID/ScoreBoard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Alvin Quijano  
+ * Date:Aug 17, 2017
+ * StudentID: 300795606 
+ * Description: This is the ScoreBoard class
+ * Version: 0.1 - Created the ScoreBoard class 
+ */
+
+namespace COMP123_S2017_FinalExam_StudentID
+{
+    class ScoreBoard
+    {
+
+    }
+}
